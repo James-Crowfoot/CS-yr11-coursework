f94aa3a3aa0cc8a0878b7da094150af8edf50d40
James C <22crowfootjames@queenelizabeths.com>
2024-11-30 08:52:10 +0000

    Update main.py
    added nice looking card display

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ import sys
 from replit import clear
 from random import randint
 from game_tools import *
+import binascii
+
+
 '''
 https://docs.google.com/document/d/10rOlHKvELJlZe1rTSGimzs3Q4rMm99l2Q7uR4lm9mt4/edit
 '''
 #EXCERSISE, INTELLIGENCE, FRIEDNLINESS, DROOL, NAME
 
+
+#ASCII DOGS
+
 dog1 = ["  __    __", "o-''))_____\\\\", "\"--__/ * * * )", "c_c__/-c____/"]
-
 dog2 = ["  .", "..^____/", "`-. ___ )", "  ||  || mh"]
-dog3 = [
-    "		    __", "(\,--------'()'--o", " (_    ___    /~\"", "  (_)_)  (_)_)"
-]
-
-for i in dog3:
-	print(i)
-debug_end()
+dog3 = ["		    __", "(\,--------'()'--o", " (_    ___    /~\"", "  (_)_)  (_)_)"]
+dog4=["	^..^      /","	/_/\\_____/","	   /\\   /\\",".     /  \\ /  \\"]
+dog5 = [".     __      _","	o'')}____//","	 `_/      )","	 (_(_/-(_/",]
+ASCIIdogs = [dog1,dog2,dog3,dog4,dog5]
 
 #VARIABLES
-debug_mode = 1  # DEBUG MODE - 1=displays messages during code operation
+debug_mode = 0  # DEBUG MODE - 1=displays messages during code operation
 failures = 0
 
 debug_log = []
@@ -60,11 +62,11 @@ for i in range(0, 30):
 	dog_card.append(friendliness)
 	dog_card.append(drool)
 	dog_card.append(name)
-	print(dog_card)
+	#print(dog_card)
 	cards.append(dog_card)
 
 file.close()
-print(cards)
+#print(cards)
 #debug_end()
 
 total_cards = len(cards)
@@ -93,8 +95,8 @@ initialisation = "unsuccessful"
 while initialisation == "unsuccessful":
 	try:
 		debug("shuffling cards...", 0)
-		shuffle_cards(cards, 1)
-		print(cards)
+		shuffle_cards(cards, 0)
+		#print(cards)
 	except Exception:
 		debug("Error", 0)
 		failures += 1
@@ -146,7 +148,7 @@ if "1" in reply or "PLAY" in reply:
 	#PLAY GAME
 	clear()
 	debug("play game --> loading", 0)
-	loading(1, 3)
+	#loading(1, 3)
 elif "2" in reply or "QUIT" in reply:
 	'''If the user selects the ‘Quit’ option then a suitable message should be 		  displayed and the program ends.'''
 	sleep(0.5)
@@ -167,7 +169,7 @@ try:
 		reply = int(input("How many cards to be played in total: "))
 		if reply <= 30 and reply > 3 and reply % 2 == 0:
 			msg = Fore.GREEN + "value accepted" + Fore.RESET
-			print(msg, 1)
+			print(msg)
 			value_acceptable = True
 		else:
 			print("must be an even number between 4 and 30 inclusive")
@@ -179,8 +181,8 @@ finally:
 	cards_each = int(cards_to_be_played / 2)
 	computer_cards = cards[0:cards_each]
 	player_cards = cards[cards_each:cards_to_be_played]
-	print(computer_cards)
-	print(player_cards)
+	#print(computer_cards)
+	#print(player_cards)
 
 debug("GAME SEGMENT", 1)
 
@@ -194,6 +196,7 @@ print("#  #### #     # #  #  # #####   ")
 print("#     # ####### #     # #       ")
 print("#     # #     # #     # #       ")
 print("#####   #     # #     # ####### ")
+print("\n________________________________\n")
 print("\n")
 
 game = True
@@ -210,21 +213,71 @@ while game == True:
 	PlayDrool = CurrentPlayerCard[3]
 	PlayName = CurrentPlayerCard[4]
 
-	BotExcersise = CurrentComputerCard[1]
-	BotIntelligence = CurrentComputerCard[2]
-	BotFriendliness = CurrentComputerCard[3]
-	BotDrool = CurrentComputerCard[4]
-	BotName = CurrentComputerCard[5]
+	BotExcersise = CurrentComputerCard[0]
+	BotIntelligence = CurrentComputerCard[1]
+	BotFriendliness = CurrentComputerCard[2]
+	BotDrool = CurrentComputerCard[3]
+	BotName = CurrentComputerCard[4]
 
-	print("-*----- celebrity dogs -----*-")
-	print(" name: ", PlayName)
-	print(" ~")
-	print(" excersise: ", PlayExcersise)
-	print(" ~")
-	print(" intelligence: ", PlayIntelligence)
-	print(" ~")
-	print(" friendliness: ", PlayFriendliness)
-	print(" ~")
-	print(" drool: ", PlayDrool)
-	print("-*--------------------------*-")
-	category = input("")
+	NameAddon=""
+	msg="|  name: "+ str(PlayName)
+	LenName=len(msg)
+	#print(LenName)
+	for i in range(0,32-LenName):
+		NameAddon+=" "
+		
+	ExcersiseAddon=""
+	msg="|  excersise: "+ str(PlayExcersise)
+	LenExcersise=len(msg)
+	#print(LenExcersise)
+	for i in range(0,32-LenExcersise):
+		ExcersiseAddon+=" "
+
+	IntelligenceAddon=""
+	msg="|  intelligence: "+ str(PlayIntelligence)
+	LenIntelligence=len(msg)
+	#print(LenIntelligence)
+	for i in range(0,32-LenIntelligence):
+		IntelligenceAddon+=" "
+
+	FriendlinessAddon=""
+	msg="|  friendliness: "+ str(PlayFriendliness)
+	LenFriendliness=len(msg)
+	#print(LenFriendliness)
+	for i in range(0,32-LenFriendliness):
+		FriendlinessAddon+=" "
+
+	DroolAddon=""
+	msg="|  drool: "+ str(PlayDrool)
+	LenDrool=len(msg)
+	#print(LenDrool)
+	for i in range(0,32-LenDrool):
+		DroolAddon+=" "
+	
+	ascii = ASCIIdogs[randint(0,4)]
+	print("\n\n-*-------- celebrity dogs --------*-")
+
+	#Dog image
+	for a in ascii:
+		DogAddon=""
+		msg="|  "+a
+		LenDog=len(msg)
+		#print(LenDog)
+		for i in range(0,32-LenDog):
+			DogAddon+=" "
+		print("|   "+a,DogAddon,"|")
+	#Dog Stats
+	print("|  ~                               |")
+	print("|  name: ", PlayName,NameAddon,"|")
+	print("|  ~                               |")
+	print("|  excersise: ", PlayExcersise,ExcersiseAddon,"|")
+	print("|  ~                               |")
+	print("|  intelligence: ", PlayIntelligence,IntelligenceAddon,"|")
+	print("|  ~                               |")
+	print("|  friendliness: ", PlayFriendliness,FriendlinessAddon,"|")
+	print("|  ~                               |")
+	print("|  drool: ", PlayDrool,DroolAddon,"|")
+	print("|  ~                               |")
+	print("-*--------------------------------*-\n")
+	
+	category = input("Category: ")
